db0529269a1ea176358ab04fe5ad4a38be9a734e
Sang Choi <sangchoi93@gmail.com>
2019-11-22 21:24:03 -0600

    utility functions to build build_ramachandran charts with torsion angles

--- utilities.py
+++ utilities.py
@@ -3,6 +3,7 @@ import numpy as np
 import json
 from scrape_pdb import pdb_parser
 from matplotlib import pyplot as plt
+from matplotlib import cm
 
 # utilities to analyze the pdb data parsed out by pdb_parser class in scrape_pdb.py
 class pdb_utilities:
@@ -23,21 +24,23 @@ class pdb_utilities:
             '9': '2-7 ribbon/hex',
             '10': 'polyproline'
         }
+        self.df_atom['new_index'] = self.df_atom.apply(lambda x: x['protein_name']+x['res_seq']+x['atom_name'], axis=1)
+        self.df_atom = self.df_atom.set_index('new_index')
+        self.dict_coordinates = self.build_coordinates_lookup()
 
     def find_coordinates_atom(self, protein_name: str, atom_name: str):
-        l_atom_name = atom_name.split('.')
-        found_atom = self.df_atom[(self.df_atom['res_seq'] == l_atom_name[0]) &
-                            (self.df_atom['atom_name'] == l_atom_name[1])]
-        if protein_name:
-            found_atom = found_atom[found_atom['protein_name'] == protein_name]
+        # l_atom_name = atom_name.split('.')
+        # found_atom = self.df_atom[(self.df_atom['res_seq'] == l_atom_name[0]) &
+        #                     (self.df_atom['atom_name'] == l_atom_name[1])]
+        # if protein_name:
+        #     found_atom = found_atom[found_atom['protein_name'] == protein_name]
+        # found_atom = self.df_atom.loc[protein_name + atom_name.replace('.', '')]
         
-        # print('{}\'s coordinate is ({}, {}, {})'.format(atom_name, 
-        #                                                 float(found_atom['x']), 
-        #                                                 float(found_atom['y']), 
-        #                                                 float(found_atom['z'])))
-        return {'x': float(found_atom['x']), 
-                'y': float(found_atom['y']), 
-                'z': float(found_atom['z'])}
+        # return {'x': float(found_atom['x']), 
+        #         'y': float(found_atom['y']), 
+        #         'z': float(found_atom['z'])}
+        c = self.dict_coordinates[protein_name + atom_name.replace('.', '')]
+        return {'x': float(c['x']), 'y': float(c['y']), 'z': float(c['z'])}
 
     @staticmethod
     def listify_coordinates(d: dict):
@@ -69,8 +72,12 @@ class pdb_utilities:
             print('Floating point error occurred!! {} {} {}'.format(protein_name, seq_aa, typ))
             return None
         except TypeError:
-            print('atom not found!', protein_name, seq_aa, typ)
+            # print('coordinates not found!', protein_name, seq_aa, typ)
             return None
+        except KeyError:
+            # print('atom not found!', protein_name, seq_aa, typ)
+            return None
+
 
     @staticmethod
     def calculate_dihedral(list_coords):
@@ -88,15 +95,26 @@ class pdb_utilities:
 
         return round(np.degrees(np.arctan2(y, x)), 3)        
 
-    def plot_ramachandran(self, df:pd.DataFrame):
+    def plot_ramachandran(self, df:pd.DataFrame, colorcode_by: str=''):
         plt.grid()
-        plt.scatter(df['psi'], df['phi'], alpha=0.5)
+        plt.xlabel('psi')
+        plt.ylabel('phi')
+        if colorcode_by:
+            classes = list(np.unique(df[colorcode_by]))
+            colors = cm.rainbow(np.linspace(0, 1, len(classes)))
+            for cl, c in zip(classes, colors):
+                df_tmp = df[df[colorcode_by] == cl]
+                plt.scatter(df_tmp['psi'], df_tmp['phi'], alpha=0.5, color=c)
+            plt.legend(classes)
+        else:
+            plt.scatter(df['psi'], df['phi'], alpha=0.5)
 
+        
     def build_ramachandran_aa(self, res_name):
         df_aa = self.df_atom[self.df_atom['res_name'] == res_name][['protein_name', 'res_seq']].drop_duplicates(keep='first')
         df_aa['psi'] = df_aa.apply(lambda x: self.calculate_angle(x['protein_name'], x['res_seq'], 'psi'), axis=1)
         df_aa['phi'] = df_aa.apply(lambda x: self.calculate_angle(x['protein_name'], x['res_seq'], 'phi'), axis=1)
-        self.plot_ramachandran(df_aa)
+        # self.plot_ramachandran(df_aa)
         return df_aa
 
     def build_ramachandran_helices(self):
@@ -110,22 +128,39 @@ class pdb_utilities:
                 for i in range(int(helix[2]), int(helix[3])+1):
                     dict_helix = {
                         'helix_type': self.type_helix[helix[1]],
-                        'helix_type_code': helix[1],
+                        'helix_class': helix[1],
                         'protein_name': helix[0],
                         'aa_seq_num': str(i),
                         'psi': self.calculate_angle(helix[0], str(i), 'psi'),
                         'phi': self.calculate_angle(helix[0], str(i), 'phi')
                     }
+                    # print(dict_helix)
                     l_dict_ramanchandra += [dict_helix]
             
         df_helices_ramanchandran = pd.read_json(json.dumps(l_dict_ramanchandra))
         return df_helices_ramanchandran
-   
+    
+    def build_coordinates_lookup(self):
+        dict_coordinates = dict()
+
+        import time
+        print('building coordinates...')
+        now = time.time()
+
+        for index, row in self.df_atom.iterrows():
+            dict_coordinates[index] = {'x': row['x'], 'y': row['y'], 'z': row['z']}
+        
+        print('build_coordinates_lookup took {}s.....'.format(time.time()-now))
+
+        return dict_coordinates
+
+
 if __name__ == '__main__':
-    pdb_parser = pdb_parser(1)
+    pdb_parser = pdb_parser(100)
     pdb_utilities = pdb_utilities(pdb_parser.df_atom, 
                                   pdb_parser.df_helix, 
                                   pdb_parser.df_sheet)
     # print(pdb_parser.df_atom[pdb_parser.df_atom['protein_name'] == '12AS'])
     # print(pdb_utilities.calculate_angle('12ASA', '327', 'psi'))
-    pdb_utilities.build_ramachandran_aa('VAL')
+    # pdb_utilities.build_ramachandran_aa('VAL')
+    print(pdb_utilities.build_ramachandran_helices())
